0e6b544fc3e9a8e9f7856b475c5d908cd7b02006
lwj920 <dhkswn15@naver.com>
2025-02-12 16:39:08 +0900

    all projects

---
+++ project01/src/js/main.js
@@ -0,0 +1,6 @@
+const btn=document.querySelector('button');
+const h1El = document.querySelector('h1');
+
+btn.addEventListener("click",()=>{
+  h1El.style.color ='royalblue';
+});
---
+++ project02/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      message : 'Hello World Vue'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
